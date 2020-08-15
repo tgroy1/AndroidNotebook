f4b24ec213139a2e0d635bd2dc6acb87fa5aeb3a
Tiyas <tgroy1@gmail.com>
2020-08-16 00:10:30 +0530

    Added multiple screens

---
+++ app/src/main/java/com/example/notebook/DataManager.kt
@@ -0,0 +1,33 @@
+package com.example.notebook
+
+object DataManager {
+    val courses = HashMap<String, CourseInfo>()
+    val notes = ArrayList<NoteInfo>()
+
+    init {
+        initializeCourses()
+        initializeNotes()
+    }
+
+    private fun initializeCourses() {
+        var course = CourseInfo("android_intent", "Android Programming with Intents")
+        courses.set(course.courseId, course)
+
+        course = CourseInfo(courseId = "android_async", title = "Android Async Programming and Services")
+        courses.set(course.courseId, course)
+
+        course = CourseInfo(title = "Java Fundamentals: The Java Language", courseId = "java_lang")
+        courses.set(course.courseId, course)
+
+        course = CourseInfo("java_core", "Java Fundamentals: The Core Platform")
+        courses.set(course.courseId, course)
+    }
+
+    private fun initializeNotes() {
+        var course = courses["android_intent"]!!
+        var note = NoteInfo(course, "Dynamic intent resolution",
+            "Wow, intents allow components to be resolved at runtime")
+        notes.add(note)
+    }
+
+}
--- app/src/main/java/com/example/notebook/MainActivity.kt
+++ app/src/main/java/com/example/notebook/MainActivity.kt
@@ -6,23 +6,38 @@ import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
-import kotlinx.android.synthetic.main.fragment_first.*
+import android.widget.ArrayAdapter
+import com.example.notebook.DataManager.notes
+import kotlinx.android.synthetic.main.content_main.*
 
 class MainActivity : AppCompatActivity() {
+    private var notePosition = POSITION_NOT_SET
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(findViewById(R.id.toolbar))
 
-        findViewById<FloatingActionButton>(R.id.fab).setOnClickListener { view ->
-            val originalValue = textDisplayedValue.text.toString().toInt()
-            val newValue = originalValue * 2
-            textDisplayedValue.text = newValue.toString()
-            Snackbar.make(view, "Value $originalValue changed to $newValue",
-                Snackbar.LENGTH_LONG)
-                 .show()
-        }
+        val adapterCourses = ArrayAdapter<CourseInfo>(this,
+            android.R.layout.simple_spinner_item,
+            DataManager.courses.values.toList())
+        adapterCourses.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+
+        spinnerCourses.adapter = adapterCourses
+
+        notePosition = intent.getIntExtra(EXTRA_NOTE_POSITION, POSITION_NOT_SET)
+
+        if (notePosition != POSITION_NOT_SET)
+            displayNote()
+    }
+
+    private fun displayNote() {
+        val note = DataManager.notes[notePosition]
+        textNoteTitle.setText(note.title)
+        textNoteText.setText(note.text)
+
+        val coursePosition = DataManager.courses.values.indexOf(note.course)
+        spinnerCourses.setSelection(coursePosition)
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
